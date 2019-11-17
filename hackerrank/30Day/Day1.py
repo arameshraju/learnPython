61622f8006b2ba3330323edd57a62c59404b4d1f
arameshraju <alluri.ramesh@gmail.com>
2019-11-17 16:10:46 +0530

    day 1 and day2

---
+++ hackerrank/30Day/Day1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+i2=int(raw_input())
+# Read and save an integer, double, and String to your variables.
+d2=float(raw_input())
+s2=raw_input()
+# Print the sum of both integer variables on a new line.
+print i+i2
+print d+d2
+print s  + " " + s2
+
