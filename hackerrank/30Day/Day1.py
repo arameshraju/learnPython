178c13951cfe3e1b38f9cf275bf5950a5bde0cee
arameshraju <alluri.ramesh@gmail.com>
2019-11-17 18:44:26 +0530

    day 1 and day2

--- hackerrank/30Day/Day1.py
+++ hackerrank/30Day/Day1.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 i = 4
 d = 4.0
 s = 'HackerRank '
 # Declare second integer, double, and String variables.
-i2=int(raw_input())
+i2=int(input())
 # Read and save an integer, double, and String to your variables.
-d2=float(raw_input())
-s2=raw_input()
+d2=float(input())
+s2=input()
 # Print the sum of both integer variables on a new line.
-print i+i2
-print d+d2
-print s  + " " + s2
+print (i+i2)
+print (d+d2)
+print (s  +   s2)
 
